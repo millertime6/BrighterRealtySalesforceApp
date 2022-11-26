6297ba8199d97b1c514535ec2fcca300d8adeb72
Scott Miller <scott@scottmillerrealestate.com>
2022-11-25 22:37:17 -0500

    Add Existing File

--- force-app/main/default/lwc/getProperty/getProperty.js
+++ force-app/main/default/lwc/getProperty/getProperty.js
@@ -1,6 +1,6 @@
 import { LightningElement,api,wire,track } from 'lwc';
 import getPropertyListingData from '@salesforce/apex/GetPropertyListing.getPropertyListingData';
-
+import LightningAlert from 'lightning/alert';
 export default class GetProperty extends LightningElement {
     @api recordId; 
     @track data;
@@ -19,6 +19,23 @@ export default class GetProperty extends LightningElement {
             console.log(this.data[0]); 
             // this.loadingSpinner = false;
         })
+    }
+    handleMobileChange(event){
+        this.mobilevalue = event.target.value;
+    } 
+
+    handleNext() {
+        
+    }
+
+    async handleCancel(){
+
+            await LightningAlert.open({
+            message: 'this is the alert message',
+            theme: 'error', 
+            label: 'Error!', 
+            variant: 'header',
+        });
     }
 }
 
