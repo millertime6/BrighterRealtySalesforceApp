4f448731e11dc345c02e02b6ff51acdb71de9ae2
Scott Miller <scott@scottmillerrealestate.com>
2022-12-30 23:07:58 -0500

    Add Existing File

--- force-app/main/default/lwc/getProperty/getProperty.js
+++ force-app/main/default/lwc/getProperty/getProperty.js
@@ -1,5 +1,7 @@
 import { LightningElement,api,track } from 'lwc';
 import getPropertyListingData from '@salesforce/apex/GetPropertyListing.getPropertyListingData';
+import LightningConfirm from 'lightning/confirm';
+
 export default class GetProperty extends LightningElement {
     @api recordId; 
     @track data;
@@ -9,8 +11,21 @@ export default class GetProperty extends LightningElement {
     disableButton() {
     this.disabled = true;
   }
+
+  handleNext() {
+    alert('Your request is processing. Please do not press the button again and refresh your screen. ');        
+}
+    async handleCancel(){
+
+      const result = LightningConfirm.open({
+          message: 'Your request is processing. You do not need to press the button again and please make sure to refresh your screen in a few seconds.',
+          variant: 'header',
+          label: 'Request Received',
+          theme: 'error',
+      });
+  }
     handleClick(){
-            // this.loadingSpinner = true;
+            this.handleCancel(); 
             this.disableButton(); 
             getPropertyListingData({subjectPropertyId: this.recordId}).then(resp => {
             // console.log('recordId: '+recordId); 
--- force-app/main/default/lwc/getSaleMortgage/getSaleMortgage.js
+++ force-app/main/default/lwc/getSaleMortgage/getSaleMortgage.js
@@ -1,5 +1,6 @@
 import { LightningElement,api,wire,track } from 'lwc';
 import getSaleAndMortgage from '@salesforce/apex/GetSaleAndMortgage.getSaleAndMortgage';
+import LightningConfirm from 'lightning/confirm';
 
 export default class getSaleMortgage extends LightningElement {
     @api recordId; 
@@ -10,8 +11,22 @@ export default class getSaleMortgage extends LightningElement {
         this.disabled = true;
       }
 
+      handleNext() {
+        alert('Your request is processing. Please do not press the button again and refresh your screen. ');        
+    }
+    async handleCancel(){
+
+        const result = LightningConfirm.open({
+            message: 'Your request is processing. You do not need to press the button again and please make sure to refresh your screen in a few seconds.',
+            variant: 'header',
+            label: 'Request Received',
+            theme: 'error',
+        });
+    }
+
     handleClick(){
             this.disableButton(); 
+            this.handleCancel(); 
             getSaleAndMortgage({subjectPropertyId: this.recordId}).then(resp => {
             this.data = JSON.parse(resp).message;
             console.log(this.data[0]); 
